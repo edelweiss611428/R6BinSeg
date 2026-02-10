a283a33d15bac9d5eff7685be0c362f66a0334a2
edelweiss611428 <edelweiss611428@gmail.com>
2026-02-10 14:26:19 +1100

    Update custom_L1_costFunc.R

--- inst/custom_L1_costFunc.R
+++ inst/custom_L1_costFunc.R
@@ -1,14 +1,15 @@
 library("R6BinSeg")
+set.seed(256)
 
 segSize = 50
 p_outl = 0.4
 x <- c(
-  rnorm(segSize, -5, 0.3) + rt(segSize, 2)*sample(c(0,1), segSize, T, p = c(p_outl, 1- p_outl)),
-  rnorm(segSize, -3, 0.3) + rt(segSize, 2)*sample(c(0,1), segSize, T, p = c(p_outl, 1- p_outl)),
-  rnorm(segSize, -1, 0.3)  + rt(segSize, 2)*sample(c(0,1), segSize, T,p = c(p_outl, 1- p_outl)),
-  rnorm(segSize, 1, 0.3)  + rt(segSize, 2)*sample(c(0,1), segSize, T, p = c(p_outl, 1- p_outl)),
-  rnorm(segSize, 3, 0.3)  + rt(segSize, 2)*sample(c(0,1), segSize, T, p = c(p_outl, 1- p_outl)),
-  rnorm(segSize, 5, 0.3) + rt(segSize, 2)*sample(c(0,1), segSize, T, p = c(p_outl, 1- p_outl))
+  rnorm(segSize, -5, 0.3) + rt(segSize, 1)*sample(c(0,1), segSize, T, p = c(p_outl, 1- p_outl)),
+  rnorm(segSize, -3, 0.3) + rt(segSize, 1)*sample(c(0,1), segSize, T, p = c(p_outl, 1- p_outl)),
+  rnorm(segSize, -1, 0.3)  + rt(segSize, 1)*sample(c(0,1), segSize, T,p = c(p_outl, 1- p_outl)),
+  rnorm(segSize, 1, 0.3)  + rt(segSize, 1)*sample(c(0,1), segSize, T, p = c(p_outl, 1- p_outl)),
+  rnorm(segSize, 3, 0.3)  + rt(segSize, 1)*sample(c(0,1), segSize, T, p = c(p_outl, 1- p_outl)),
+  rnorm(segSize, 5, 0.3) + rt(segSize, 1)*sample(c(0,1), segSize, T, p = c(p_outl, 1- p_outl))
 )
 
 f <- function(s,e){
@@ -27,7 +28,7 @@ bs_l2$fit()
 
 par(mfrow = c(1,2))
 plot(x, type = "l", main = "binSeg (L1)")
-abline(v = bs$predict(5), col = "red", lwd = 2)
+abline(v = bs$predict(10), col = "red", lwd = 2)
 plot(x, type = "l", main = "binSeg (L2)")
-abline(v = bs_l2$predict(5), col = "red", lwd = 2)
+abline(v = bs_l2$predict(10), col = "red", lwd = 2)
 par(mfrow = c(1,1))
